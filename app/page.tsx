c9b876720452606060ce9226c58fe375efa1a836
KevinParmar867 <kevinparmar097@gmail.com>
2025-09-11 16:28:14 +0530

    add inter font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="min-h-screen grid place-items-center">
-      <h1>medfaster-portal</h1>
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+
+        <div className="flex gap-3">
+          <Button>Default</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="outline">Outline</Button>
+        </div>
+        <div className="flex gap-3">
+          <span className="font-normal">Regular</span>{" "}
+          <span className="font-semibold">Semibold</span>{" "}
+          <span className="font-[150]">150 weight</span>
+          <span className="font-[250]">250 weight</span>
+          <span className="font-[350]">350 weight</span>
+          <span className="font-[450]">450 weight</span>
+          <span className="font-[550]">550 weight</span>
+          <span className="font-[650]">650 weight</span>
+          <span className="font-[750]">750 weight</span>
+          <span className="font-[850]">850 weight</span>
+          <span className="font-[950]">950 weight</span>
+        </div>
+      </main>
     </div>
   );
 }
